84caedf7055ae3de0947121be656ed536afba3b6
brewchetta <chett.tiller@gmail.com>
2025-02-03 11:20:13 -0500

    pens!

--- server/app.py
+++ server/app.py
@@ -2,12 +2,42 @@
 
 from flask import request, make_response, jsonify
 from config import app, db
-from models import ???
-
-# HELPERS ######################
-
+from models import Pen
 
 # ROUTES #######################
+
+@app.get('/pens')
+def all_pens():
+    # step one: get all the pens
+    all_pens = Pen.query.all()
+    # step two: make them into dictionaries
+    pen_dicts = [ pen.to_dict() for pen in all_pens ] # list comprehension
+    # step three: return it
+    return pen_dicts, 200
+
+@app.post('/pens')
+def post_pen():
+    try: # try to do this
+        # step one: get body data
+        body = request.json
+        # step two: make the pen object
+        new_pen = Pen(
+            color=body.get('color'),
+            clickable=body.get('clickable'),
+            length=body.get('length'),
+            brand=body.get('brand'),
+            price=body.get('price')
+        )
+        db.session.add(new_pen)
+        db.session.commit()
+        # step three: return it
+        return new_pen.to_dict(), 201
+    
+    except ValueError as e: # if there is a specific validation error do this
+        return { "error": str(e) }, 400
+
+    except Exception as e: # if there is a generic error then do this
+        return { "error": str(e), "image": "Some gif url goes here" }, 400
 
 
 # RUN ##########################
--- server/models.py
+++ server/models.py
@@ -4,3 +4,47 @@ from sqlalchemy.orm import validates
 
 
 # MODELS ################
+
+class Pen(db.Model, SerializerMixin):
+
+    __tablename__ = 'pens_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    brand = db.Column(db.String, nullable=False) # the nullable here doesn't do much anymore bc of the validation
+    color = db.Column(db.String)
+    clickable = db.Column(db.Boolean, default=False)
+    length = db.Column(db.Float)
+    price = db.Column(db.Float)
+
+    # VALIDATIONS
+    @validates('brand') # which attributes are we validating here?
+    def validate_brand(self, key, new_value):
+        # if this is not good throw an error
+        if not isinstance(new_value, str):
+            raise ValueError('Pen brand must be a string')
+        if len(new_value) < 2:
+            raise ValueError('Pen brand must be at least two characters')
+        # otherwise return the new_value
+        return new_value
+
+    @validates('length', 'price')
+    def validate_are_positive(self, key, new_value): # key is either 'price' or 'length'
+        if not isinstance(new_value, int) and not isinstance(new_value, float):
+            raise ValueError(f"Pen {key} must be an integer or float")
+        if new_value <= 0:
+            raise ValueError(f'Pen {key} must be a positive value')
+        return new_value
+    
+    @validates('color')
+    def validate_color(self, key, new_value):
+        colors = ('red', 'blue', 'black', 'orange') # valid pen colors
+        if not new_value.lower() in colors:
+            raise ValueError('Pen color must be red, blue, black, or orange')
+        return new_value.lower() # this will lowercase the value as it goes into the db
+    
+    @validates('clickable')
+    def validate_clickable(self, key, new_value):
+        if not isinstance(new_value, bool):
+            raise ValueError('Pen clickable must either be True or False')
+        return new_value
